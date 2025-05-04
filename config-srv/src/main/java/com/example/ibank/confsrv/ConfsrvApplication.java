0fc67c67dcd121d105764bca74d5cfe38df9581e
mao321 <mao321-ru@users.noreply.github.com>
2025-05-04 18:22:14 +0400

    [+] Добавлен стартовый проект для Spring Cloud Config Server

---
+++ config-srv/src/main/java/com/example/ibank/confsrv/ConfsrvApplication.java
@@ -0,0 +1,13 @@
+package com.example.ibank.confsrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfsrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfsrvApplication.class, args);
+	}
+
+}
---
+++ config-srv/src/test/java/com/example/ibank/confsrv/ConfsrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ibank.confsrv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfsrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
