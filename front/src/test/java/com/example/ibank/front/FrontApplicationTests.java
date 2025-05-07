96fe1a0695c7b5d8a41ed86078f0458ce38a58c3
mao321 <mao321-ru@users.noreply.github.com>
2025-05-08 01:16:04 +0400

    [+] Добавлен модуль для Front UI (front)

---
+++ front/src/test/java/com/example/ibank/front/FrontApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.ibank.front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class FrontApplicationTests extends IntegrationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ front/src/test/java/com/example/ibank/front/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.example.ibank.front;
+
+import com.example.ibank.common.IntegrationTestBase;
+
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+
+// Общие настройки для всех интеграционных тестов модуля
+@TestPropertySource( properties = "spring.config.import=configserver:http://localhost:8913")
+public abstract class IntegrationTest extends IntegrationTestBase {
+
+    static {
+        startContainers( 8913, List.of( Container.EUREKA));
+    }
+
+}
